7c7eb2e0608c4e8190adf20ea6cbe4b26f15e3b5
Drnyash <tugumbajhadiza@gmail.com>
2024-11-02 01:03:03 +0600

    Add files via upload

---
+++ HW/HW31.10/script.js
@@ -0,0 +1,13 @@
+var num = prompt("1 inshi san: ")
+var num2 = prompt("2 inshi san: ")
+var num3 = prompt("3 inshi san: ")
+
+var arifmetikalikOrta
+var man
+
+arifmetikalikOrta = (num + num2 + num3);
+
+man = arifmetikalikOrta / 3
+
+
+alert("arifmetikalikOrta: " +man)
